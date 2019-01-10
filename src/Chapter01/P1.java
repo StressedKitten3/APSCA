e41cede98ff2ae20c94bae526fc9ad3f24dc717a
KeylonKolton <>
2019-01-10 08:50:49 -0600

    Chapter 01 - Chapter 04

---
+++ src/Chapter01/P1.java
@@ -0,0 +1,17 @@
+package Chapter01;
+/**
+ * Program to say welcome to java
+ * 
+ * @author KoltonKeylon
+ */
+public class P1 {
+/**
+ * Main method
+ * 
+ * @param args command line arguments
+ */
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+    }
+}
